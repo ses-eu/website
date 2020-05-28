90c32fd17575580487d51072f6ee72e0a4e1a03d
Holger Hegendoerfer <holger.hegendorfer@eurocontrol.int>
2020-05-28 17:47:21 +0200

    update of 2019 safety data

---
+++ static/javascript/2019/crt_EoSMYY_ST_19.js
@@ -0,0 +1,59 @@
+(function() {
+    function dv(){
+
+  // EoSM - YY - CHART - [EU_YY]
+  var crt_EoSMYY_ST = new google.visualization.ChartWrapper({
+    chartType: 'ComboChart',
+    containerId: 'crt_EoSMYY_ST_19',
+    dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1kp_CFuFfUVLk_4krFAzpOCuJURH6OhIKUm0YiAzAMu0/edit?usp=sharing&sheet=EoSM_State_Score&range=A1:G31',
+    options: {
+        width: 475,
+        height: 230,
+        chartArea: {
+            left: 50,
+            top: 30,
+            width: '85%',
+            height: '50%'
+        },
+        title: 'Effectiveness of Safety Management score (State level)',
+        titleTextStyle: {
+            color: 'grey',
+            fontSize: 12
+        },
+        legend: 'none',
+        isStacked: 'true',
+        seriesType: 'bars',
+        colors: ['#7DD2F2'],
+        vAxis: {
+            title: 'EoSM score',
+            titleTextStyle: {
+                color: 'grey',
+                fontSize: 11
+            }
+        },
+        hAxis: {
+            title: 'EASA verified results',
+            titleTextStyle: {
+                color: 'grey',
+                fontSize: 11
+            },
+            type: 'string',
+            slantedText: true,
+            slantedTextAngle: 90,
+            textStyle: {
+                color: 'grey',
+                fontSize: 10
+            }
+        }
+    },
+    view: {
+        'columns': [0, 6]
+    }
+});
+crt_EoSMYY_ST.draw();
+        
+
+    }
+
+    google.setOnLoadCallback(dv);
+ })();
