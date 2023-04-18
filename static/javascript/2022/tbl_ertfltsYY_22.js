4df83e50c568a25d7d2129dee22e26c283726e06
Holger Hegendoerfer <holger.hegendorfer@eurocontrol.int>
2023-04-18 18:20:52 +0200

    Updates 2022 full year

--- static/javascript/2022/tbl_ertfltsYY_22.js
+++ static/javascript/2022/tbl_ertfltsYY_22.js
@@ -5,7 +5,7 @@
     var tbl_ertflts_YY = new google.visualization.ChartWrapper({
         chartType: 'Table',
         containerId: 'tbl_ertfltsYY_22',
-        dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1J6R5sJfdlaend7mr9BrzPNJ4yGwFNqK29vLsLwVzJn4/edit?usp=sharing&sheet=ERT_FLTS_LOC&range=A5:F34',
+        dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1J6R5sJfdlaend7mr9BrzPNJ4yGwFNqK29vLsLwVzJn4/edit?usp=sharing&sheet=ERT_FLTS_LOC&range=A5:F35',
         options: {
             allowHtml: false,
             width: 470,
