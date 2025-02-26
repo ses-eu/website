2bdb861e6743d958c56dbf944bf1dbe855314a09
Holger Hegendoerfer <holger.hegendorfer@eurocontrol.int>
2025-02-26 17:25:43 +0100

    2024 DB updated with full ASMA and taxi out

--- static/javascript/2024/tbl_txdly_APT_24.js
+++ static/javascript/2024/tbl_txdly_APT_24.js
@@ -1,11 +1,11 @@
 (function() {
     function dv(){
-     
+       
         // ENVIRONMENT - TAXI OUT DLY - APT - TABLE
     var tbl_txdly_APT = new google.visualization.ChartWrapper({
         chartType: 'Table',
         containerId: 'tbl_txdly_APT_24',
-        dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1PmqdVqGdFrlPEOlznaIxZada3uy2Kwl2nBWNFyk4Btc/edit?usp=sharing&sheet=TxOut_APT&range=A5:G47',
+        dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1mqWQkV0vddQTZwB0e44fUAYF0SKY65BMfFl8asGcJbA/edit?usp=sharing&sheet=TxOut_APT&range=A5:G47',
         options: {
             allowHtml: false,
             width: 950,
